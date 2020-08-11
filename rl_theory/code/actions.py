b5a6bbfd82563421d12154b365d373357c1fab59
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-08-10 19:15:34 -0700

    Finish first Makefile.

--- rl_theory/code/actions.py
+++ rl_theory/code/actions.py
@@ -3,7 +3,7 @@ import sys
 
 out = ""
 
-filename = sys.argv[1]
+filename = sys.argv[1] + ".dat"
 print(filename)
 actions = np.loadtxt(filename, dtype=int)
 for row in range(actions.shape[0]):
@@ -11,5 +11,5 @@ for row in range(actions.shape[0]):
         idx = (row * 3) + col + 1
         out += "\\defl{{action{}}}{{{}}}\n".format(idx, actions[row][col])
 
-with open("temp_actions.tex", "w") as file:
+with open("_input/" + sys.argv[1] + ".tex", "w") as file:
     file.write(out)
