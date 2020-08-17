99a5f220138ae3291f4331600aa0855198f30520
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-08-17 11:38:53 -0700

    Put header paths on all files.

--- scripts/path_fmt.py
+++ scripts/path_fmt.py
@@ -1,7 +1,7 @@
 import sys
 
 path = sys.argv[1]
-path = path.replace("proof", "\\proofd").replace("rl_theory/","")
+path = path.replace("proof", "\\proofd") #.replace("rl_theory/","")
 path = path.replace("_", "\\_")
 path = "{{\\tt {}}}".format(path)
 print(path)
