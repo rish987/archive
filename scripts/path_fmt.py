fd1b71f6824cb347d54a190473dab5dd9bbeb193
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-08-23 16:01:20 -0700

    Fix "first link" functionality.

--- scripts/path_fmt.py
+++ scripts/path_fmt.py
@@ -12,12 +12,12 @@ for dir_i, dir in enumerate(path_split):
         path_split_fmt.append("\\noted")
     else:
         link = dir.replace("_", "\\_")
-        #link = "\\hlc[lightlightgray]{{{}}}".format(link)
-        if dir_i < len(path_split) - 1:
-            link_path = "/".join(path_split[0:dir_i + 3]) + "_"
-        else:
-            link_path = "/".join(path_split[0:dir_i + 1])
-        link = "\\lnraw{{{}}}{{{}}}".format(link_path, link)
+        if sys.argv[2] == "F":
+            if dir_i < len(path_split) - 1:
+                link_path = "/".join(path_split[0:dir_i + 3]) + "_"
+            else:
+                link_path = "/".join(path_split[0:dir_i + 1])
+            link = "\\lnraw{{{}}}{{{}}}".format(link_path, link)
 
         path_split_fmt.append(link)
 
