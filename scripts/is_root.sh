07508ffbfe8982ffdc4da0e873fd7a164e5ee4d9
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-08-26 10:57:39 -0700

    Adjust vim link-follower for refactoring.

--- scripts/is_root.sh
+++ scripts/is_root.sh
@@ -1,9 +1,9 @@
-roots=("./rl_theory")
-roots+=(`find . -type d \( -name "proof" -o -name "note" \) | xargs -i find "{}" -maxdepth 1 -mindepth 1 -type d`)
+roots=("src/rl_theory")
+roots+=(`find src -type d \( -name "proof" -o -name "note" \) | xargs -i find "{}" -maxdepth 1 -mindepth 1 -type d`)
 
 roots=(`realpath ${roots[@]}`)
 
-if [[ " ${roots[@]} " =~ "$1 " ]]; then
+if [[ " ${roots[@]} " =~ " $1 " ]]; then
     exit 0
 fi
 exit 1
