28901ce4e45f57c5874025d4801cad1b06360e61
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-09-19 20:32:50 -0700

    Rename to 'archive'.

--- archive.mk
+++ archive.mk
@@ -1,6 +1,6 @@
 # --- auxilliary dependencies ---
 # --- --- rl_theory --- ---
-RL_T_D := ${BUILD_SOURCE_DIR}/archives/topic/rl_theory
+RL_T_D := ${BUILD_SOURCE_DIR}/archive/topic/rl_theory
 RL_T_INP := ${RL_T_D}/_input/
 RL_T_CODE := ${RL_T_D}/code/
 RL_T_INP_ACT := $(addprefix ${RL_T_INP},actions_1.tex actions_2.tex)
@@ -9,7 +9,7 @@ RL_T_INP_REW := $(addprefix ${RL_T_INP},rewards.tex)
 RL_T_CODE_ACT := $(addprefix ${RL_T_CODE},actions.py)
 RL_T_CODE_REW := $(addprefix ${RL_T_CODE},rewards.py)
 
-$(addsuffix /archives/topic/rl_theory/ref.pdf,${TREE_DIRS}) ${ARCHIVES_F} ${ARCHIVES_F_C}: ${RL_T_INP_ACT} ${RL_T_INP_REW} $(addprefix ${RL_T_D}/parts/,notation.tex example.tex preliminaries.tex)
+${OUTPUT_DIR}/tree/archive/topic/rl_theory/tree.pdf ${OUTPUT_DIR}/tree_online/archive/topic/rl_theory/tree_online.pdf ${OUTPUT_DIR}/full/archive/full.pdf ${OUTPUT_DIR}/full_compact/archive/full_compact.pdf : ${RL_T_INP_ACT} ${RL_T_INP_REW} $(addprefix ${RL_T_D}/parts/,notation.tex example.tex preliminaries.tex)
 
 ${RL_T_D}/_input/actions_%.tex : ${RL_T_CODE_ACT} ${RL_T_D}/code/actions_%.dat | ${RL_T_INP}
 	cd $(dir $<); python $(notdir $<) actions_$*
