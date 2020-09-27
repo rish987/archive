f195cf0cf4b828ea1104d2b83e6015484475a84b
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2020-09-27 11:27:33 -0700

    Restructure rl_theory as a topic of prod.

--- archive.mk
+++ archive.mk
@@ -1,6 +1,7 @@
 # --- auxilliary dependencies ---
 # --- --- rl_theory --- ---
-RL_T_D := ${BUILD_SOURCE_DIR}/archive/topic/rl_theory
+RL_T_D_R := archive/topic/prob/topic/rl_theory
+RL_T_D := ${BUILD_SOURCE_DIR}/${RL_T_D_R}
 RL_T_INP := ${RL_T_D}/_input/
 RL_T_CODE := ${RL_T_D}/code/
 RL_T_INP_ACT := $(addprefix ${RL_T_INP},actions_1.tex actions_2.tex)
@@ -9,7 +10,7 @@ RL_T_INP_REW := $(addprefix ${RL_T_INP},rewards.tex)
 RL_T_CODE_ACT := $(addprefix ${RL_T_CODE},actions.py)
 RL_T_CODE_REW := $(addprefix ${RL_T_CODE},rewards.py)
 
-${OUTPUT_DIR}/tree/archive/topic/rl_theory/tree.pdf ${OUTPUT_DIR}/tree_online/archive/topic/rl_theory/tree_online.pdf ${OUTPUT_DIR}/full/archive/full.pdf ${OUTPUT_DIR}/full_compact/archive/full_compact.pdf : ${RL_T_INP_ACT} ${RL_T_INP_REW} $(addprefix ${RL_T_D}/parts/,notation.tex example.tex preliminaries.tex)
+${OUTPUT_DIR}/tree/${RL_T_D_R}/tree.pdf ${OUTPUT_DIR}/tree_online/${RL_T_D_R}/tree_online.pdf ${OUTPUT_DIR}/full/archive/full.pdf ${OUTPUT_DIR}/full_compact/archive/full_compact.pdf : ${RL_T_INP_ACT} ${RL_T_INP_REW} $(addprefix ${RL_T_D}/parts/,notation.tex example.tex preliminaries.tex)
 
 ${RL_T_D}/_input/actions_%.tex : ${RL_T_CODE_ACT} ${RL_T_D}/code/actions_%.dat | ${RL_T_INP}
 	cd $(dir $<); python $(notdir $<) actions_$*
